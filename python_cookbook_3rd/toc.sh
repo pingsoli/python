3a48543e79a572bc18484aa5af79a0194aeb6f20
16.04.3 <pingsoli@163.com>
2017-12-21 22:52:16 +0800

    test

--- python_cookbook_3rd/toc.sh
+++ python_cookbook_3rd/toc.sh
@@ -7,7 +7,7 @@ prefix="https://github.com/pingsoli/python/blob/master/python_cookbook_3rd"
 for dir in $directories
 do
   text=$(head -n 1 $dir/$target)
-  link=$(echo $text | tr '[A-Z]' '[a-z]' | sed 's/ //; s/,/ /g; s/  */-/g')
+  link=$(echo $text | xargs | tr '[A-Z]' '[a-z]' | sed 's/ //; s/,/ /g; s/  */-/g')
   url="$prefix/$(basename $dir)$link"
   echo "[$text]($url)    " >> $target
 done
