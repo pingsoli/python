755036e76325088e3fe20e988bd2250adbe4bf7c
16.04.3 <pingsoli@163.com>
2017-12-30 11:38:39 +0800

    python decorators

--- advanced/decorators.py
+++ advanced/decorators.py
@@ -3,12 +3,10 @@
 # =====================================================================
 
 def my_decorator(some_function):
-
     def wrapper():
         print('something is happening before some_function() is called')
         some_function()
         print('something is happening after some_function() is called')
-
     return wrapper
 
 def just_some_function():
@@ -26,11 +24,9 @@ just_some_function = my_decorator(just_some_function)
 import time
 
 def timing_function(some_function):
-
     '''
     Output the time a funtion takes
     '''
-
     def wrapper():
         start = time.time()
         some_function()
@@ -53,11 +49,9 @@ def my_function():
 from time import sleep
 
 def sleep_decorator(function):
-
     '''
     Limits how fast the function is called
     '''
-
     def wrapper(*args, **kwargs):
         sleep(2)
         return function(*args, **kwargs)
@@ -71,8 +65,119 @@ def print_number(num):
 
 # =====================================================================
 
+def get_next(name):
+    return 'lorem ipsum, {0} dolor sit meat'.format(name)
 
+def p_decorate(func):
+    def func_wrapper(name):
+        return '<p>{0}</p>'.format(func(name))
+    return func_wrapper
+
+my_get_text = p_decorate(get_next)
+#print(my_get_text('John'))
+# <p>lorem ipsum, John dolor sit meat</p>
 
 # =====================================================================
 
+# Use Python Decorator
+def p_decorate(func):
+    def func_wrapper(name):
+        return '<p>{0}</p>'.format(func(name))
+    return func_wrapper
 
+@p_decorate
+def get_next(name):
+    return 'lorem ipsum, {0} dolor sit meat'.format(name)
+
+#print(get_next('pingsoli'))
+# <p>lorem ipsum, pingsoli dolor sit meat</p>
+
+# =====================================================================
+
+# Next Example Using Multiple Decorator
+def p_decorate(func):
+    def func_wrapper(name):
+        return '<p>{0}</p>'.format(func(name))
+    return func_wrapper
+
+def strong_decorate(func):
+    def func_wrapper(name):
+        return '<strong>{0}</strong>'.format(func(name))
+    return func_wrapper
+
+def div_decorate(func):
+    def func_wrapper(name):
+        return '<div>{0}</div>'.format(func(name))
+    return func_wrapper
+
+@div_decorate
+@strong_decorate
+@p_decorate
+def get_next(name):
+    return 'lorem ipsum, {0} dolor sit meat.'.format(name)
+
+#print(get_next('pingsoli'))
+# <div><strong><p>lorem ipsum, pingsoli dolor sit meat.</p></strong></div>
+
+# =====================================================================
+
+# Decorating Methods
+def p_decorate(func):
+    def func_wrapper(name):
+        return '<p>{0}</p>'.format(func(name))
+    return func_wrapper
+
+class Person:
+    def __init__(self):
+        self.name = 'pingsoli'
+        self.family = 'Lee'
+
+    @p_decorate
+    def get_fullname(self):
+        return self.name + ' ' + self.family
+
+my_person = Person()
+#print(my_person.get_fullname())
+# <p>pingsoli Lee</p>
+
+# =====================================================================
+
+# A much better approach 
+def p_decorate(func):
+    def func_wrapper(*args, **kwargs):
+        return '<p>{0}</p>'.format(func(*args, **kwargs))
+    return func_wrapper
+
+class Person:
+    def __init__(self):
+        self.name = 'pingsoli'
+        self.family = 'Lee'
+
+    @p_decorate
+    def get_fullname(self):
+        return self.name + ' ' + self.family
+
+my_person = Person()
+#print(my_person.get_fullname())
+# <p>pingsoli Lee</p>
+
+# =====================================================================
+
+# Passing arguments to decorators
+def tags(tag_name):
+    def tags_decorator(func):
+        def func_wrapper(name):
+            return '<{0}>{1}</{0}>'.format(tag_name, func(name))
+        return func_wrapper
+    return tags_decorator
+
+@tags('div')
+@tags('strong')
+@tags('p')
+def get_next(name):
+    return 'hello ' + name
+
+print(get_next('pingsoli'))
+# <div><strong><p>hello pingsoli</p></strong></div>
+
+# =====================================================================
